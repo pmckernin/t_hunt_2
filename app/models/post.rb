b3dcbb3a239e376dd282a9e634068e769fdaead9
pmckernin <pmckernin@gmail.com>
2020-01-09 17:30:12 -0600

    had some edits

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+    validates :user_id, presence: true
+    validates :reward, presence: true
+    validates :date_lost, presence: true
+    validates :post, presence: true
+    
+
+
 end
